6311b62bc66a272acaf7e54d96937dec3bf45ee0
MatthewAndresWestMec <111930814+MatthewAndresWestMec@users.noreply.github.com>
2022-11-16 20:09:23 -0700

    calculator

---
+++ calculator.js
@@ -0,0 +1,32 @@
+//addds number using respective function + input parameters
+function num(num) 
+{
+    document.getElementById("input").value +=num;
+}
+
+//signs are added to the equation/ input
+function add()
+{
+    document.getElementById("input").value +="+";
+}
+function subtract()
+{
+    document.getElementById("input").value +="-";
+}
+function divide()
+{
+    document.getElementById("input").value +="/";
+}
+function multiply()
+{
+    document.getElementById("input").value +="*";
+}
+//input clear + solve
+function deleteInput()
+{
+    document.getElementById("input").value = "";
+}
+function equal()
+{
+    document.getElementById("input").value = eval(document.getElementById("input").value)
+}
